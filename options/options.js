6fd91c9e59023d8db64378e2e80fce8924c3e53b
Crowdbotics <team@crowdbotics.com>
2023-09-18 00:33:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "punkpal-43429",
+  url: "https://punkpal-43429.botics.co",
+  api: "https://punkpal-43429.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
